e2777664c0f1efaab16f69b24d7295bcd6647024
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-06-29 15:48:30 +0500

    road-design

--- src/Layout/Footer/Footer.jsx
+++ src/Layout/Footer/Footer.jsx
@@ -8,25 +8,27 @@ import etherscan from "../../Assets/etherscan.png";
 import logo from "../../Assets/logo3.png";
 function Footer() {
   return (
-    <footer>
-      <div className="logo">
-        <img src={logo} alt="" />
-      </div>
-      <div className="social-div">
-        <a href="https://twitter.com/solsistersclub" target={"blank"}>
-          <FontAwesomeIcon icon={faTwitter} className={"header-icon"} />
-        </a>
-        <a href="https://twitter.com/solsistersclub" target={"blank"}>
-          <img src={opensea} alt="" className="header-icon" />
-        </a>
-        <a
-          href="https://etherscan.io/address/0x108c5ee2be6472e2110482801e86c520bbd40bfe"
-          target={"blank"}
-        >
-          <img src={etherscan} alt="" className="header-icon" />
-        </a>
-      </div>
-    </footer>
+    <div className="footer-cont">
+      <footer>
+        <div className="logo">
+          <img src={logo} alt="" />
+        </div>
+        <div className="social-div">
+          <a href="https://twitter.com/solsistersclub" target={"blank"}>
+            <FontAwesomeIcon icon={faTwitter} className={"header-icon"} />
+          </a>
+          <a href="https://twitter.com/solsistersclub" target={"blank"}>
+            <img src={opensea} alt="" className="header-icon" />
+          </a>
+          <a
+            href="https://etherscan.io/address/0x108c5ee2be6472e2110482801e86c520bbd40bfe"
+            target={"blank"}
+          >
+            <img src={etherscan} alt="" className="header-icon" />
+          </a>
+        </div>
+      </footer>
+    </div>
   );
 }
 
--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -11,88 +11,90 @@ function Header() {
   const [menu, setMenu] = useState(false);
 
   return (
-    <div className="header">
-      <div className="social-div">
-        <a href="https://twitter.com/solsistersclub" target={"blank"}>
-          <FontAwesomeIcon icon={faTwitter} className={"header-icon"} />
-        </a>
-        <a href="https://twitter.com/solsistersclub" target={"blank"}>
-          <img src={opensea} alt="" className="header-icon" />
-        </a>
-        <a
-          href="https://etherscan.io/address/0x108c5ee2be6472e2110482801e86c520bbd40bfe"
-          target={"blank"}
-        >
-          <img src={etherscan} alt="" className="header-icon" />
-        </a>
-      </div>
-      <FontAwesomeIcon
-        onClick={() => setMenu(true)}
-        icon={faBars}
-        className="menu-icon mobile"
-      />
-
-      <div className="logo">
-        {/* <h1 className="lulu-bold">sol SISTERS</h1> */}
-        <img src={logo} alt="" />
-      </div>
-      <div className="menu-icon-div desktop">
+    <div className="header-cont">
+      <div className="header">
+        <div className="social-div">
+          <a href="https://twitter.com/solsistersclub" target={"blank"}>
+            <FontAwesomeIcon icon={faTwitter} className={"header-icon"} />
+          </a>
+          <a href="https://twitter.com/solsistersclub" target={"blank"}>
+            <img src={opensea} alt="" className="header-icon" />
+          </a>
+          <a
+            href="https://etherscan.io/address/0x108c5ee2be6472e2110482801e86c520bbd40bfe"
+            target={"blank"}
+          >
+            <img src={etherscan} alt="" className="header-icon" />
+          </a>
+        </div>
         <FontAwesomeIcon
           onClick={() => setMenu(true)}
           icon={faBars}
-          className="menu-icon"
+          className="menu-icon mobile"
         />
-      </div>
-      <div className={`slide-menu ${menu ? "open-slide-menu" : ""}`}>
-        <FontAwesomeIcon
-          onClick={() => setMenu(false)}
-          icon={faXmark}
-          className="cross"
-        />
-        {/* <div className="row-menu">
+
+        <div className="logo">
+          {/* <h1 className="lulu-bold">sol SISTERS</h1> */}
+          <img src={logo} alt="" />
+        </div>
+        <div className="menu-icon-div desktop">
+          <FontAwesomeIcon
+            onClick={() => setMenu(true)}
+            icon={faBars}
+            className="menu-icon"
+          />
+        </div>
+        <div className={`slide-menu ${menu ? "open-slide-menu" : ""}`}>
+          <FontAwesomeIcon
+            onClick={() => setMenu(false)}
+            icon={faXmark}
+            className="cross"
+          />
+          {/* <div className="row-menu">
           <img src={menuImg} alt="" />
           <h2 className="lulu-bold">MENU</h2>
         </div> */}
-        <div className="menu-links-div">
-          <a
-            href="#hero"
-            onClick={() => setMenu(false)}
-            className="futura menu-link active"
-          >
-            Home
-          </a>
+          <div className="menu-links-div">
+            <a
+              href="#hero"
+              onClick={() => setMenu(false)}
+              className="futura menu-link active"
+            >
+              Home
+            </a>
 
-          <a
-            href="#about"
-            onClick={() => setMenu(false)}
-            className="futura menu-link"
-          >
-            About Us
-          </a>
+            <a
+              href="#about"
+              onClick={() => setMenu(false)}
+              className="futura menu-link"
+            >
+              About Us
+            </a>
 
-          <a
-            href="#road"
-            onClick={() => setMenu(false)}
-            className="futura menu-link"
-          >
-            Roadmap
-          </a>
+            <a
+              href="#road"
+              onClick={() => setMenu(false)}
+              className="futura menu-link"
+            >
+              Roadmap
+            </a>
 
-          <a
-            href="#team"
-            onClick={() => setMenu(false)}
-            className="futura menu-link"
-          >
-            Team
-          </a>
+            <a
+              href="#team"
+              onClick={() => setMenu(false)}
+              className="futura menu-link"
+            >
+              Team
+            </a>
 
-          <a
-            href="#faq"
-            onClick={() => setMenu(false)}
-            className="futura menu-link"
-          >
-            Faq
-          </a>
+            <a
+              href="#faq"
+              onClick={() => setMenu(false)}
+              className="futura menu-link"
+            >
+              Faq
+            </a>
+          </div>
         </div>
       </div>
     </div>
