f322b8ae7fd14196526a9bbcc298d7bcab08cc5a
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-06-29 16:35:59 +0500

    road-design

--- src/Pages/Home/AboutArt/Art.jsx
+++ src/Pages/Home/AboutArt/Art.jsx
@@ -7,13 +7,11 @@ function Art() {
   return (
     <div className="art">
       <Fade triggerOnce direction="up" duration={1000} delay={200}>
+        <h1 className="section-heading kanit">About The Art</h1>
         <div className="team-grid__box">
-          <div className="team-box__left">
-            <h1 className="section-heading kanit">About The Art</h1>
-          </div>
           <div className="team-box__right">
             {/* <img src={gif} alt="" className="gif-box" /> */}
-            <p className="team-box-desc futura">
+            <p className="team-box-desc lulu">
               VC villaflores has drawn a total of 178 traits to ensure that each
               and every sister is unique and beautiful, these traits are divided
               in to 12 categories ( Type, Eyes, Mouth, Hair, Head item,
